b12d9c75bac9d511476b9e0a4ad246ca60a0ee8b
abkein <rickbatra0z@gmail.com>
2024-11-27 18:20:48 +0300

    Debug

--- src/dump_vector.h
+++ src/dump_vector.h
@@ -7,8 +7,8 @@ DumpStyle(vector,DumpVector);
 #ifndef LMP_DUMP_VECTOR_H
 #define LMP_DUMP_VECTOR_H
 
-#include "dump.h"
 #include "compute.h"
+#include "dump.h"
 
 namespace LAMMPS_NS {
 
@@ -18,19 +18,24 @@ class DumpVector : public Dump {
   ~DumpVector() override;
 
  protected:
-  Compute **computes; // array to store pointers to the vector data computes
-  int num_computes;   // number of computes
-  double *vector_data; // pointer to store vector data
-  int write_cutoff;    // number of elements to write
+  Compute **computes;     // array to store pointers to the vector data computes
+  int num_computes;       // number of computes
+  double *vector_data;    // pointer to store vector data
+  int write_cutoff;       // number of elements to write
 
   void init_style() override;
   void write_header(bigint) override;
   void pack(tagint *) override;
   void write_data(int, double *) override;
-  void openfile(int compute_index); // New method to open file based on compute index
+  void openfile(int compute_index);    // New method to open file based on compute index
+  template <typename TYPE> inline TYPE **create_ptr_array(TYPE **&array, int n, const char *name)
+  {
+    array = n <= 0 ? nullptr : static_cast<TYPE **>(memory->smalloc(sizeof(TYPE *) * n, name));
+    return array;
+  }
 };
 
-} // namespace LAMMPS_NS
+}    // namespace LAMMPS_NS
 
 #endif
 #endif
